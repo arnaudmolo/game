a7d7c5a345015c2a5a9c55a341d53c7c30b5c2ce
onesime <arnaud@onesime.fr>
2013-04-03 13:58:39 +0200

    En fait ca a pas bien push o/

---
+++ js/collection/gallery.js
@@ -0,0 +1,20 @@
+// Collection
+var hetic = hetic || {};
+
+(function(){
+	"use strict";
+	var Gallery = Backbone.Collection.extend({
+		model: hetic.Student,
+		initialize: function(){
+			this.on('add', this.render, this);
+			this.on('edit', this.render, this);
+		},
+		render: function(model){
+			$("#student-list").prepend( model.view.render().el );
+		}
+	});
+	hetic.gallery = new Gallery();
+	hetic.gallery.create({firstname: 'Anthony', lastname: 'Zaccardi', image: 'img/zaccardi.png', promotion: 2015, website:"http://www.anthonyzaccardi.com/", capacities: ['html']});
+	hetic.gallery.create({firstname: 'Arnaud', lastname: 'Pichon', image: 'img/arnaud.png', promotion: 2015, website:"http://www.onesime.fr", capacities: ['html', 'backbone']});
+	hetic.gallery.create({firstname: 'Mathieu', lastname: 'Dutto', image: 'img/mathieu.jpg', promotion: 2015, website:"http://www.mathieudutto.fr", capacities: ['html', 'css', 'php']});
+}());
---
+++ js/model/student.js
@@ -0,0 +1,35 @@
+// MODEL STUDENT
+var hetic = hetic || {};
+
+(function(){
+	"use strict";
+	hetic.Student = Backbone.Model.extend({
+		defaults: {
+			firstname: '',
+			lastname:'',
+			capacities: [],
+			image: '',
+			promotion: 2015,
+			show: 'true',
+			website: ''
+		},
+		initialize: function(){
+			this.view = new hetic.StudentView({model: this});
+		},
+		sync: function(){},
+		hasCapacity: function(search){
+			if (search==='') {
+				this.view.switchClass(true);
+				return;
+			}
+			var el = false;
+			_.each(this.get("capacities"), function(element){
+				if (element === search) {
+					el = true;
+				}
+			});
+			this.view.switchClass(el);
+			return el;
+		}
+	});
+}());
